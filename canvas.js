3a2dd576855d82e3ad7a57bba3a842632bac5a91
Shubhayu-Das <shubhayu64@gmail.com>
2019-09-05 12:14:35 +0530

    Adding styling features

--- canvas.js
+++ canvas.js
@@ -42,148 +42,172 @@
 
 // Canvas State Variables
 canvas = {
-    ctx: undefined,
-    height: undefined,
-    width: undefined,
-    mouseX: 0,
-    mouseY: 0,
-    mouseDown: false,
-    mouseDownX: 0,
-    mouseDownY: 0,
-    keysDown: {},
-}
+	ctx: undefined,
+	height: undefined,
+	width: undefined,
+	mouseX: 0,
+	mouseY: 0,
+	mouseDown: false,
+	mouseDownX: 0,
+	mouseDownY: 0,
+	keysDown: {},
+	fillMode: undefined,
+};
 
 // Canvas Setup function
-canvas.setup = function () {
-    // extract canvas object and set attributes correctly
-    this.ctx = document.getElementById("canvasArea").getContext("2d");
-    this.height = window.innerHeight;
-    this.width = window.innerWidth;
+canvas.setup = function() {
+	// extract canvas object and set attributes correctly
+	this.ctx = document.getElementById("canvasArea").getContext("2d");
+	this.height = window.innerHeight;
+	this.width = window.innerWidth;
 
-    document.getElementById("canvasArea").width = this.width;
-    document.getElementById("canvasArea").height = this.height;
+	document.getElementById("canvasArea").width = this.width;
+	document.getElementById("canvasArea").height = this.height;
 
-    // Start listeners
-    this.startListeners();
+	// Start listeners
+	this.startListeners();
 
-    // Complete custom setup
-    this.setupFunction();
-}
+	// Complete custom setup
+	this.setupFunction();
+
+	// Set the default fill mode to Stroke
+	this.setMode();
+};
+
+// @author Shubhayu Das
+// Set the mode to Fill/Stroke
+canvas.setMode = function(mode = "stroke") {
+	if (mode === "fill") {
+		this.ctx.fillMode = this.ctx.fill;
+	} else {
+		this.ctx.fillMode = this.ctx.stroke;
+	}
+};
+
+// @author Shubhayu Das
+// Change the color of the shape(s) drawn unless set to some other color
+canvas.setColor = function(color) {
+	this.ctx.fillStyle = color;
+	this.ctx.strokeStyle = color;
+};
+
+// @author Shubhayu Das
+// Sets the thickness of the line of the shapes in strokes
+canvas.setLineThickness = function(width = 1) {
+	this.ctx.lineWidth = width;
+};
 
 // Draws line from (x1, y1) to (x2, y2)
 canvas.drawLine = function(x1, y1, x2, y2) {
-    this.ctx.beginPath();
-    this.ctx.moveTo(x1, y1);
-    this.ctx.lineTo(x2, y2);
-    this.ctx.stroke();
-}
+	this.ctx.beginPath();
+	this.ctx.moveTo(x1, y1);
+	this.ctx.lineTo(x2, y2);
+	this.ctx.fillMode();
+};
 
 // Draws circle with center (x, y) and radius r
 canvas.drawCircle = function(x1, y1, r) {
-    this.ctx.beginPath();
-    this.ctx.arc(x1, y1, r, 0, 2 * Math.PI);
-    this.ctx.stroke();
-}
+	this.ctx.beginPath();
+	this.ctx.arc(x1, y1, r, 0, 2 * Math.PI);
+	this.ctx.fillMode();
+};
 
 // Draws rectangle with top left corner as (x, y) and of dimensions width * height
 canvas.drawRectangle = function(x, y, width, height) {
-    this.ctx.beginPath();
-    this.ctx.rect(x, y, width, height);
-    this.ctx.stroke();
-}
+	this.ctx.beginPath();
+	this.ctx.rect(x, y, width, height);
+	this.ctx.fillMode();
+};
 
-// Draws <message> at (x, y) 
+// Draws <message> at (x, y)
 canvas.drawText = function(x, y, message, fontSize = 30) {
-    this.ctx.font = fontSize + "px Arial";
-    this.ctx.fillText(message, x, y);
-}
+	this.ctx.font = fontSize + "px Arial";
+	this.ctx.fillText(message, x, y);
+};
 
-// Clear canvase
+// Clear canvas
 canvas.clear = function() {
-    this.ctx.clearRect(0, 0, this.width, this.height);
-}
+	this.ctx.clearRect(0, 0, this.width, this.height);
+};
 
 // Function which will setup calling of canvas.mainFunction every <timeStep> milliseconds
-canvas.startMain =  function(timeStep = 50) {
-    setInterval(this.mainFunction, timeStep);
-}
+canvas.startMain = function(timeStep = 50) {
+	setInterval(this.mainFunction, timeStep);
+};
 
 // Will start event listeners
-// An event listener is a procedure or function in a computer program that waits for an event to occur. 
+// An event listener is a procedure or function in a computer program that waits for an event to occur.
 // Examples of an event are the user clicking or moving the mouse, pressing a key on the keyboard
-canvas.startListeners = function () {
+canvas.startListeners = function() {
+	window.addEventListener("mousemove", e => {
+		this.mouseX = e.clientX;
+		this.mouseY = e.clientY;
+		this.mouseMoveCallback(e);
+	});
 
-    window.addEventListener('mousemove', (e) => {
-        this.mouseX = e.clientX;
-        this.mouseY = e.clientY;
-        this.mouseMoveCallback(e);
-    });
+	window.addEventListener("mousedown", () => {
+		this.mouseDownX = this.mouseX;
+		this.mouseDownY = this.mouseY;
+		this.mouseDown = true;
+		this.mouseDownCallback();
+	});
 
-    window.addEventListener("mousedown", () => {
-        this.mouseDownX = this.mouseX; 
-        this.mouseDownY = this.mouseY; 
-        this.mouseDown = true;
-        this.mouseDownCallback();
-    });
+	window.addEventListener("mouseup", () => {
+		this.mouseDown = false;
+		this.mouseUpCallback();
+	});
 
-    window.addEventListener("mouseup", () => { 
-        this.mouseDown = false;
-        this.mouseUpCallback();
-    });
+	window.addEventListener("keyup", e => {
+		console.log(e.code + " was released");
+		this.keysDown[e.code] = false;
+		this.keyUpCallback(e);
+	});
 
-    window.addEventListener('keyup', (e) => {
-        console.log(e.code + " was released" );
-        this.keysDown[e.code] = false;
-        this.keyUpCallback(e);
-    });
+	window.addEventListener("keydown", e => {
+		console.log(e.code + " was pressed");
+		this.keysDown[e.code] = true;
+		this.keyDownCallback(e);
+	});
+};
 
-    window.addEventListener('keydown', (e) => {
-        console.log(e.code + " was pressed" );
-        this.keysDown[e.code] = true;
-        this.keyDownCallback(e);
-    });
-}
-
-// Checks if keyboard key is pressed. Example KeyA for A. 
+// Checks if keyboard key is pressed. Example KeyA for A.
 canvas.isKeyDown = function(key) {
-    return keysDown[key] == true;
-}
-
+	return keysDown[key] == true;
+};
 
 // Functions Users Can Override in index.html
 
 // Main function which is called every <timeStep> milliseconds when canvas.startMain() is called
-canvas.mainFunction = function () {
-    console.log("Dummy Main Function - Override canvas.mainFunction");
-}
+canvas.mainFunction = function() {
+	console.log("Dummy Main Function - Override canvas.mainFunction");
+};
 
 // Custom Setup function for setting up global variables
 canvas.setupFunction = function() {
-    console.log("Dummy Setup Function - Override canvas.setupFunction");
-}
+	console.log("Dummy Setup Function - Override canvas.setupFunction");
+};
 
 // Called when key is pressed
-canvas.keyDownCallback = function (e) {
-    console.log("Dummy KeyDownCallback - Override canvas.KeyDownCallback");
-}
+canvas.keyDownCallback = function(e) {
+	console.log("Dummy KeyDownCallback - Override canvas.KeyDownCallback");
+};
 
 // Called when key is released
-canvas.keyUpCallback = function (e) {
-    console.log("Dummy keyUpCallback - Override canvas.keyUpCallback");
-}
+canvas.keyUpCallback = function(e) {
+	console.log("Dummy keyUpCallback - Override canvas.keyUpCallback");
+};
 
 // Called when mouse is pressed
-canvas.mouseDownCallback = function () {
-    console.log("Dummy mouseDownCallback - Override canvas.mouseDownCallback");
-}
+canvas.mouseDownCallback = function() {
+	console.log("Dummy mouseDownCallback - Override canvas.mouseDownCallback");
+};
 
 // Called when mouse is released
-canvas.mouseUpCallback = function () {
-    console.log("Dummy mouseUpCallback - Override canvas.mouseUpCallback");
-}
+canvas.mouseUpCallback = function() {
+	console.log("Dummy mouseUpCallback - Override canvas.mouseUpCallback");
+};
 
 // Called when mouse is moved
-canvas.mouseMoveCallback = function (e) {
-    // console.log("Dummy mouseMoveCallback - Override canvas.mouseMoveCallback");
-}
-
+canvas.mouseMoveCallback = function(e) {
+	// console.log("Dummy mouseMoveCallback - Override canvas.mouseMoveCallback");
+};
