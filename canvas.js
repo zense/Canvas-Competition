794114702c02ff838d8b10ac36391612dbc6864c
Prajwal7842 <prajwalgoel@gmail.com>
2019-09-08 16:18:21 +0530

    Added feature to draw Ellipse

--- canvas.js
+++ canvas.js
@@ -1,7 +1,8 @@
 /*
     Developed for Zense Hackathon
     Author - Satvik Ramaprasad,
-             Shubhayu Das
+             Shubhayu Das,
+             Prajwal Agarwal.
 
     Note - DO NOT CHANGE CONTENTS OF THIS FILE
 
@@ -37,10 +38,10 @@
         canvas.drawLine(x1, y1, x2, y2) // Draws line from (x1, y1) to (x2, y2)
         canvas.drawCircle(x, y, r) // Draws circle with center (x, y) and radius r
         canvas.drawRectangle(x, y, width, height) // Draws rectangle with top left corner as (x, y) and of dimensions width * height
+        canvas.drawEllipse(x, y, a, b, angle, startAngle, endAngle) // Draws an Ellipse with center(x,y) with major axis = a, minor axis = b at an angle theta given the startAngle And Endangle
         canvas.clear() // Clears the canvas
         canvas.isKeyDown(key) // Checks if keyboard key is pressed. Example KeyA for A. 
         canvas.drawText(x, y, message, fontSize = 30) // Draws <message> at (x, y) 
-
 
     Optionally Override the following functions
         canvas.mouseDownCallback() // Called when mouse is pressed
@@ -131,11 +132,21 @@ canvas.drawRectangle = function(x, y, width, height) {
     this.draw();
 }
 
+//Draws Ellipse 
+canvas.drawEllipse = function(x, y, radiusX, radiusY, angle, startAngle, endAngle){
+    endAngle = (endAngle/180.0)*Math.PI;
+    this.ctx.beginPath();
+    this.ctx.ellipse(x, y, radiusX, radiusY, angle, startAngle, endAngle, true);
+    this.draw();
+    this.ctx.closePath();
+}
+
 // Draws <message> at (x, y) 
 canvas.drawText = function(x, y, message, fontSize = 30) {
     this.ctx.font = fontSize + "px Arial";
     this.ctx.fillText(message, x, y);
 }
+
 
 // Clear canvase
 canvas.clear = function() {
@@ -225,4 +236,3 @@ canvas.mouseUpCallback = function () {
 canvas.mouseMoveCallback = function (e) {
     // console.log("Dummy mouseMoveCallback - Override canvas.mouseMoveCallback");
 }
-
